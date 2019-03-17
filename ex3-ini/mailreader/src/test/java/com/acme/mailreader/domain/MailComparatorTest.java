ecdc6488edac1d3827be45e2a7e471bf061d9986
aulinerze <rouzepau@gmail.com>
2019-03-17 12:08:38 +0100

    maj exo4

--- ex3-ini/mailreader/src/test/java/com/acme/mailreader/domain/MailComparatorTest.java
+++ ex3-ini/mailreader/src/test/java/com/acme/mailreader/domain/MailComparatorTest.java
@@ -1,7 +1,11 @@
 package com.acme.mailreader.domain;
 
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
+import static org.hamcrest.core.IsNull.nullValue;
 import static org.junit.Assert.assertThat;
+
+import java.time.Instant;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -32,5 +36,28 @@ public class MailComparatorTest {
 		assertThat(comparator.compare(mail1, mail2), is(0));
 	}
 	
+	@Test
+	public final void mailLuEnPremier() {
+		Mail mail1 = new Mail.Builder("sujet").statut(Mail.Statut.READ).build();
+		Mail mail2 = new Mail.Builder("sujet").statut(Mail.Statut.UNSENT).build();
+		assertThat(mail1, not(nullValue()));
+		assertThat(comparator.compare(mail1, mail2), is(MailComparator.PREMIER_PLUS_GRAND));		
+	}
 	
+	@Test
+	public final void mailSujetTrieOrdreAlphabetique() {
+		Mail mail1 = new Mail.Builder("Asujet").important(true).statut(Mail.Statut.UNSENT).build();
+		Mail mail2 = new Mail.Builder("Bsujet").important(true).statut(Mail.Statut.UNSENT).build();
+		assertThat(mail1, not(nullValue()));
+		assertThat(comparator.compare(mail1, mail2), is(MailComparator.PREMIER_PLUS_GRAND));		
+	}
+	
+	@Test
+	public final void mailDateRecenteEnPremier() throws DateIncorrecteException {
+		Mail mail1 = new Mail.Builder("sujet").date(Instant.now()).build();
+		Mail mail2 = new Mail.Builder("sujet").date(Instant.now().plusSeconds(360000)).build();
+		assertThat(mail1, not(nullValue()));
+		assertThat(comparator.compare(mail1, mail2), is(MailComparator.PREMIER_PLUS_GRAND));		
+	}	
+		
 }
